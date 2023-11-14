ffc2499cfebb7692620ecd6e4333ab18ea445bc2
Kim Weast <weastkim3@gmail.com>
2023-11-13 18:35:25 -0600

    update

--- app.js
+++ app.js
@@ -53,10 +53,65 @@ class Library {
     }
 }
 
-const library = new Library();
+const Library = new Library();
 
 const submitBtn = document.getElementById('submitBtn');
 submitBtn.addEventListener('click', () => {
     library.addBook();
 });
-
+// Define your Book constructor
+function Book(title, author, read) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+  }
+  
+  // Define your library object
+  const Library = {
+    bookCount: 0,
+    addBook: function (title, author, read) {
+      // Create a new Book instance
+      const newBook = new Book(title, author, read);
+  
+      // Create a new table row
+      const newRow = document.createElement("tr");
+  
+      // Create new table cells for title, author, and read properties
+      const titleCell = document.createElement("td");
+      const authorCell = document.createElement("td");
+      const readCell = document.createElement("td");
+  
+      // Set the content of the table cells
+      titleCell.textContent = newBook.title;
+      authorCell.textContent = newBook.author;
+      readCell.textContent = newBook.read ? "Yes" : "No";
+  
+      // Append table cells to the row
+      newRow.appendChild(titleCell);
+      newRow.appendChild(authorCell);
+      newRow.appendChild(readCell);
+  
+      // Select the table body
+      const tableBody = document.getElementById("libraryBody");
+  
+      // Append the new row to the table body
+      tableBody.appendChild(newRow);
+  
+      // Increment library bookCount by one
+      this.bookCount++;
+    }
+  };
+  
+  // Add event listener to the "Add Book" button
+  const addBookBtn = document.getElementById("addBookBtn");
+  addBookBtn.addEventListener("click", function () {
+    // Get input values from the form
+    const titleInput = document.getElementById("title").value;
+    const authorInput = document.getElementById("author").value;
+    const readCheckbox = document.getElementById("read").value;
+    const readValue = readCheckbox.checked;
+  
+    // Call the library's addBook method with input values
+    library.addBook(titleInput, authorInput, readValue);
+  });
+  
